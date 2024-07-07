e02d20158eb2352c33edc15d4ef5065572bcc956
uzair-16 <uzairnaumanbeg@gmail.com>
2024-07-07 22:40:51 +0500

    change

--- hello.py
+++ hello.py
@@ -1,4 +1,33 @@
 # This how we write comments in Python using Hastag (#)
 
 
-#print("Hi Im From Karachi\n ");
+#print("Hi !\r I am from Tokyo\n ")
+#print("Hi !\t I am from Tokyo\n ")
+#print("Hi !\a I am from Tokyo\n ")
+#print("Hi !\f I am from Tokyo\n")
+#print("Hi !\v I am from Tokyo\n")
+#print("Hi !\b I am from Tokyo\n ")
+#print("Hi !\n I am from Tokyo\n ")
+ 
+#First_name= "Uzair"
+#Last_name = "Nauman"
+
+#full_name = First_name + " " + Last_name
+#print("Hello"+ "" + full_name)
+
+#print(type(name))
+#print("Hello"+ name)
+
+
+#age = 22 
+#age+= 3
+#print(" Your Age is :  " + str(age) )
+#print(type(age))
+
+#height = 276.65
+#print("Your Height is : "+ str(height))
+#print(type(height))
+
+Human = True
+print("Are you A Human : " + str(Human)) 
+print(type(Human))
